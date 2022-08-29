92b2559c19227f9eacf2f15a075e55ebedd1b3d7
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-08-29 16:06:57 +0530

    Added Models Room and Participants

--- videocall/models.py
+++ videocall/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+    name=models.CharField(blank=False,max_length=100)
+    uid=models.CharField(blank=False,max_length=200)
+    room_Name=models.CharField(blank=False,max_length=200)
+    joinedAt=models.TimeField(auto_now_add=True,editable=False)
+    cameraOn=models.BooleanField(default=True)
+    micOn=models.BooleanField(default=True)
+    screenOn=models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.name)
+
+class Room(models.Model):
+    name=models.CharField(blank=False,max_length=200)
+    user=models.ForeignKey(Participant,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
--- videocall/views.py
+++ videocall/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view,permission_classes
 # Create your views here.
+@api_view(['POST'])
 def welcome(request):
     return JsonResponse({"Message":"Welcome to PyCalling Backend"})
+
