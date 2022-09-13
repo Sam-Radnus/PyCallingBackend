954684ec19404947b7602a93bebcd5763b3c8400
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-09-13 21:40:22 +0530

    Set Debug to True

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+doye2&=zqncc(2x#3u7hszl5r8f05&o4he#tiyb50wqd15arb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pycalling.herokuapp.com','127.0.0.1']
 
@@ -123,10 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-
-
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
