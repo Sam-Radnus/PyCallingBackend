c60a1f2ba6fdfe1ed41401161037b64ec0eb94d6
samsundar <samradnus2001@gmail.com>
2022-09-14 22:01:44 +0530

    Allowed all origins

--- backend/settings.py
+++ backend/settings.py
@@ -131,9 +131,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-]
+CORS_ORIGIN_ALLOW_ALL = True
